4ba1f928a25c11508fb0e2281a6960872677f0ba
unknown <you@example.comlambealsaad@gmail.com>
2020-05-16 01:36:49 +0530

    trying lwip default iperf

--- LWIP/L6-Iperf_Server/LWIP_Default/LWIP/Target/lwipopts.h
+++ LWIP/L6-Iperf_Server/LWIP_Default/LWIP/Target/lwipopts.h
@@ -87,6 +87,8 @@
 #define DEFAULT_ACCEPTMBOX_SIZE 6
 /*----- Value in opt.h for RECV_BUFSIZE_DEFAULT: INT_MAX -----*/
 #define RECV_BUFSIZE_DEFAULT 2000000000
+/*----- Default Value for LWIP_PERF: 0 ---*/
+#define LWIP_PERF 1
 /*----- Value in opt.h for LWIP_STATS: 1 -----*/
 #define LWIP_STATS 0
 /*----- Value in opt.h for CHECKSUM_GEN_IP: 1 -----*/
