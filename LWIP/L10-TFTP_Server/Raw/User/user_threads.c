328a59e2cd8859189bc504d1727f05d5c29dfff6
unknown <you@example.comlambealsaad@gmail.com>
2020-05-20 17:32:50 +0530

    trying tftf

---
+++ LWIP/L10-TFTP_Server/Raw/User/user_threads.c
@@ -0,0 +1,85 @@
+
+#include "main.h"
+#include "cmsis_os.h"
+#include "lwip.h"
+#include "usart.h"
+#include "gpio.h"
+#include "string.h"
+
+#include "lwip/api.h"
+#include "tftpserver.h"
+
+extern struct netif gnetif;
+
+
+void Print_Char(char c)
+{
+  HAL_UART_Transmit(&huart6, (uint8_t *)&c, 1, 100);
+}
+
+void Print_String(char *str)
+{
+  uint8_t len = strlen(str);
+  HAL_UART_Transmit(&huart6, (uint8_t *)str, len, 2000);
+}
+
+void Print_IP(uint32_t ip)
+{
+  char buff[20] = {0};
+  uint8_t bytes[4];
+  bytes[3] = ip & 0xFF;
+  bytes[2] = (ip >> 8) & 0xFF;
+  bytes[1] = (ip >> 16) & 0xFF;
+  bytes[0] = (ip >> 24) & 0xFF;
+  sprintf(buff, "%d.%d.%d.%d\n", bytes[3], bytes[2], bytes[1], bytes[0]);
+  HAL_UART_Transmit(&huart6, (uint8_t *)buff, strlen(buff), 1000);
+}
+
+void dhcpPollTask(void *argument)
+{
+  uint8_t got_ip_flag = 0;
+  struct dhcp *dhcp;
+
+  Print_String("DHCP client started\n");
+  Print_String("Acquiring IP address\n");
+
+  for (;;)
+  {
+    osDelay(100);
+
+    if (got_ip_flag == 0)
+    {
+      if (dhcp_supplied_address(&gnetif))
+      {
+        got_ip_flag = 1;
+        Print_String("\ngot IP:");
+        Print_IP(gnetif.ip_addr.addr);
+
+        /* Initialize the TFTP server */
+        tftpd_init();
+      }
+      else
+      {
+        Print_Char('.');
+
+        dhcp = (struct dhcp *)netif_get_client_data(&gnetif, LWIP_NETIF_CLIENT_DATA_INDEX_DHCP);
+
+        /* DHCP timeout */
+        if (dhcp->tries > 4)
+        {
+          /* Stop DHCP */
+          dhcp_stop(&gnetif);
+          Print_String("\nCould not acquire IP address. DHCP timeout\n");
+
+          osThreadSuspend(NULL);
+        }
+      }
+    }
+  }
+}
+
+void Add_User_Threads()
+{
+  /* creat a new task to check if got IP */
+  sys_thread_new("dhcpPollTask", dhcpPollTask, NULL, 256, osPriorityNormal);
+}
